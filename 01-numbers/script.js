b00c3637f9d8a3b19b7e24c588b1ad879b8d03f1
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-11 16:34:50 +0200

    ADD solution to numbers

---
+++ 01-numbers/script.js
@@ -0,0 +1,30 @@
+const numOne = 8; // NON MODIFICARE QUESTA RIGA
+const numTwo = 16; // NON MODIFICARE QUESTA RIGA
+const numThree = 32; // NON MODIFICARE QUESTA RIGA
+
+// Sostituisci NaN di seguito utilizzando
+// gli operatori numerici di Javascript
+
+// 1. Imposta questa variabile come somma di numOne e numTwo
+const numOnePlusNumTwo = numOne + numTwo; //number
+console.log("somma n1 + n2 = ", numOnePlusNumTwo); // Output: 24
+
+// 2. Imposta questa variabile come prodotto di numThree e numTwo
+const numThreeTimesNumTwo = numThree * numTwo;
+console.log("prodotto n3 * n2 = ", numThreeTimesNumTwo); // Output: 512
+
+// 3. Imposta questa variabile come divisione di numThree per numOne
+const numThreeDividedByNumOne = numThree / numOne;
+console.log("divisione n3 / n1 = ", numThreeDividedByNumOne); // Output: 4
+
+// 4. Imposta questa variabile come differenza tra numThree e numOne
+const numThreeMinusNumOne = numThree - numOne;
+console.log("differenza n3 - n1 = ", numThreeMinusNumOne); // Output: 24
+
+// 5. Imposta questa variabile come somma di numOne, numTwo e numThree
+const sum = numOne + numTwo + numThree;
+console.log("somma di n1 + n2 + n3 = ", sum); // Output: 56
+
+// 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
+const numBytes = (numOne + numTwo + numThree) / numOne;
+console.log("somma di n1 + n2 + n3 diviso n1 = ", numBytes); // Output: 7
